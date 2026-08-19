9199400cb9a3ea4b0cb76fb0c04030fa3b0c0771
AncientSeer <nadakudutiganesh@gmail.com>
2026-08-19 23:34:03 +0530

    implemented regular expressions

--- 01-log-anomaly-detector/detector.py
+++ 01-log-anomaly-detector/detector.py
@@ -1,4 +1,7 @@
 import os
+import re
+
+log_pattern = re.compile(r'^(\S+) .*? "\S+ \S+ .*?" (\d{3})')
 
 def checkerror(code:str) -> bool:
        return code.startswith(('4','5'))
@@ -8,11 +11,11 @@ def detect_anomalies(filepath:str,threshold:int) -> dict:
           hash = {}
           with open(filepath,'r') as fh:
             for line in fh:
-             parsed_line = line.split(' ')
-             if len(parsed_line) < 9:
+             parsed_line = re.search(log_pattern,line)
+             if not parsed_line:
                 continue
-             ip = parsed_line[0]
-             code = parsed_line[8]
+             ip = parsed_line.group(1)
+             code = parsed_line.group(2)
              if checkerror(code):
                if ip in hash:
                     hash[ip]+=1
@@ -22,9 +25,10 @@ def detect_anomalies(filepath:str,threshold:int) -> dict:
 
 
 if __name__ == '__main__':
-        if os.path.exists('server.log') !=True:
-             exit()
+        if os.path.exists(r'server.log') !=True:
+             print('Log file not found')
+             exit(1)
         threshold = int(input('Enter threshold:\n'))     
-        flagged = detect_anomalies('server.log',threshold)     
+        flagged = detect_anomalies(r'server.log',threshold)     
         for ip,key in  flagged.items():
              print(f'ip:{ip} | Errors:{key}') 
