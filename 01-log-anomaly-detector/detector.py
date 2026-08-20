88dd43b6c370c63053f587767749b2c4bd4e40b0
AncientSeer <nadakudutiganesh@gmail.com>
2026-08-21 02:08:22 +0530

    implemented argparse CLI interface

--- 01-log-anomaly-detector/detector.py
+++ 01-log-anomaly-detector/detector.py
@@ -1,5 +1,7 @@
 import os
 import re
+import argparse
+
 
 log_pattern = re.compile(r'^(\S+) .*? "\S+ \S+ .*?" (\d{3})')
 
@@ -24,11 +26,21 @@ def detect_anomalies(filepath:str,threshold:int) -> dict:
           return {ip:key for ip,key in hash.items() if key>=threshold}
 
 
+def main():
+          parser = argparse.ArgumentParser(description='CLI tool to detect IP addresses with high 4xx/5xx error rates in server logs') 
+          parser.add_argument("-f","--file",required = True, help="Path to the server log file") 
+          parser.add_argument("-t","--threshold",type=int,default=3,help="Threshold for the number of errors to flag an IP address")
+          args = parser.parse_args()
+          if os.path.exists(args.file) !=True:
+                           print('Log file not found')
+                           exit(1)
+          flagged = detect_anomalies(args.file,args.threshold)    
+          if not flagged:
+                   print('No anomalies detected')
+          else: 
+                for ip,key in  flagged.items():
+                   print(f'ip:{ip} | Errors:{key}') 
+
 if __name__ == '__main__':
-        if os.path.exists(r'server.log') !=True:
-             print('Log file not found')
-             exit(1)
-        threshold = int(input('Enter threshold:\n'))     
-        flagged = detect_anomalies(r'server.log',threshold)     
-        for ip,key in  flagged.items():
-             print(f'ip:{ip} | Errors:{key}') 
+        main()
+       
